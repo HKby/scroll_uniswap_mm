d782fc39f452e66393534da3fcb9739267aad950
HKby <desearyuan@gmail.com>
2024-05-16 20:41:07 +0800

    market maker

---
+++ mm.js
@@ -0,0 +1,77 @@
+const ethers = require('ethers');
+require("dotenv").config();
+
+// Uniswap V3 - my own contract deployment on router and quoter
+const wethAddress = '0x5300000000000000000000000000000000000004'; // scroll weth
+const routerAddress = '0x10fcDc4F9e43cAD6e1Af82bF573794d716A3FBA6'; // Uniswap Router
+const quoterAddress = '0x20F7B3165b84AC964932402c826C9F6e235760bb'; // Uniswap Quoter
+const tokenAddress = '0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4'; // scroll usdc
+
+const fee = 3000; // Uniswap pool fee bps 500, 3000, 10000
+const buyAmount = ethers.parseUnits('0.001', 'ether');
+const targetPrice = BigInt(35); // target exchange rate
+const targetAmountOut = buyAmount * targetPrice;
+const sellAmount = buyAmount / targetPrice;
+const tradeFrequency = 3600 * 1000; // ms (once per hour)
+
+
+const provider = new ethers.JsonRpcProvider(process.env.SCROLL_URL);
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+const account = wallet.connect(provider);
+
+const token = new ethers.Contract(
+  tokenAddress,
+  [
+    'function approve(address spender, uint256 amount) external returns (bool)',
+    'function allowance(address owner, address spender) public view returns (uint256)',
+  ],
+  account
+);
+
+const router = new ethers.Contract(
+  routerAddress,
+  ['function exactInputSingle((address tokenIn, address tokenOut, uint24 fee, address recipient, uint256 deadline, uint256 amountIn, uint256 amountOutMinimum, uint160 sqrtPriceLimitX96)) external payable returns (uint256 amountOut)'],
+  account
+);
+
+const quoter = new ethers.Contract(
+  quoterAddress,
+  ['function quoteExactInputSingle(address tokenIn, address tokenOut, uint24 fee, uint256 amountIn, uint160 sqrtPriceLimitX96) public view returns (uint256 amountOut)'],
+  account
+);
+
+const buyTokens = async () => {
+  console.log('Buying Tokens')
+  const deadline = Math.floor(Date.now() / 1000) + 600;
+  const tx = await router.exactInputSingle([wethAddress, tokenAddress, fee, wallet.address, deadline, buyAmount, 0, 0], {value: buyAmount});
+  await tx.wait();
+  console.log(tx.hash);
+}
+
+const sellTokens = async () => {
+  console.log('Selling Tokens')
+  const allowance = await token.allowance(wallet.address, routerAddress);
+  console.log(`Current allowance: ${allowance}`);
+  if (allowance < sellAmount) {
+    console.log('Approving Spend (bulk approve in production)');
+    const atx = await token.approve(routerAddress, sellAmount);
+    await atx.wait();
+  }
+  const deadline = Math.floor(Date.now() / 1000) + 600;
+  const tx = await router.exactInputSingle([tokenAddress, wethAddress, fee, wallet.address, deadline, sellAmount, 0, 0]);
+  await tx.wait();
+  console.log(tx.hash);
+}
+
+const checkPrice = async () => {
+  const amountOut = await quoter.quoteExactInputSingle(wethAddress, tokenAddress, fee, buyAmount, 0);
+  console.log(`Current Exchange Rate: ${amountOut.toString()}`);
+  console.log(`Target Exchange Rate: ${targetAmountOut.toString()}`);
+  if (amountOut < targetAmountOut) buyTokens();
+  if (amountOut > targetAmountOut) sellTokens();
+}
+
+checkPrice();
+setInterval(() => {
+  checkPrice();
+}, tradeFrequency);
